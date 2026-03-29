f13f7aa60a373347d78232a1c0ead5ee02e9a132
Soujanya02V <soujanyabailawad@gmail.com>
2026-03-29 22:52:39 +0530

    local host setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>LitSync</h1>
-      <button>Create Folder</button>
+      {/* <button>Create Folder</button> */}
     </div>
   );
 }
